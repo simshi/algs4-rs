e0ea4a434c3cba9dd4c133171a522680529ccfbc
simshi <simonshi@gmail.com>
2019-12-11 18:42:32 +0800

    complete binary search ST

--- src/searching/binary_search_st.rs
+++ src/searching/binary_search_st.rs
@@ -42,13 +42,25 @@ impl<K: Eq + Ord, V> BinarySearchST<K, V> {
     }
     pub fn put(&mut self, key: K, value: V) {
         let up = self.upper_bound(&key);
-        if up >= self.keys.len() {
+        if up > 0 && self.keys[up - 1] == key {
+            self.values[up - 1] = value;
+        } else if up >= self.keys.len() {
             self.keys.push(key);
             self.values.push(value);
         } else {
             self.keys.insert(up, key);
             self.values.insert(up, value);
         }
+    }
+
+    pub fn contains(&self, key: &K) -> bool {
+        // contain method is O(n)
+        //self.keys.contains(key)
+        let lo = self.lower_bound(key);
+        lo < self.len() && &self.keys[lo] == key
+    }
+    pub fn rank(&self, key: &K) -> usize {
+        self.lower_bound(key)
     }
 
     pub fn floor(&self, key: &K) -> Option<&K> {
@@ -58,6 +70,12 @@ impl<K: Eq + Ord, V> BinarySearchST<K, V> {
         self.keys.get(self.upper_bound(key) - 1)
     }
 
+    pub fn delete(&mut self, key: &K) {
+        let lo = self.lower_bound(key);
+        if lo < self.len() && &self.keys[lo] == key {
+            self.keys.remove(lo);
+        }
+    }
     pub fn delete_min(&mut self) {
         if self.len() > 0 {
             self.keys.remove(0);
@@ -72,14 +90,15 @@ impl<K: Eq + Ord, V> BinarySearchST<K, V> {
         }
     }
 
-    pub fn keys(&self) -> std::slice::Iter<K> {
+    // TODO: not sure whether it was the right way to implement in
+    pub fn keys(&self) -> impl Iterator<Item = &K> {
         self.keys.iter()
     }
-    // pub fn keys_range(&self, lo: &K, hi: &K) {
-    //     let lo = self.lower_bound(lo);
-    //     let hi = self.upper_bound(hi);
-    //     self.keys.take(hi).skip(lo)
-    // }
+    pub fn keys_range<'a>(&'a self, lo: &K, hi: &K) -> impl Iterator<Item = &K> + 'a {
+        let lo = self.lower_bound(lo);
+        let hi = self.upper_bound(hi);
+        self.keys.iter().take(hi).skip(lo)
+    }
 
     pub fn check(&self) -> bool {
         self.keys.windows(2).all(|w| w[0] <= w[1])
@@ -145,6 +164,16 @@ mod tests {
     }
 
     #[test]
+    fn update() {
+        let mut st = BinarySearchST::<String, usize>::new();
+        st.put("iphone".into(), 500);
+        st.put("iphone".into(), 250);
+        assert_eq!(1, st.len());
+        assert_eq!(Some(&250), st.get(&"iphone".into()));
+        assert!(st.contains(&"iphone".into()));
+    }
+
+    #[test]
     fn get_one_of_three() {
         let mut st = BinarySearchST::<String, usize>::new();
         st.put("iphone".into(), 600);
@@ -156,10 +185,13 @@ mod tests {
         assert_eq!(Some(&800), st.get(&"blackberry".into()));
         assert!(st.check());
 
-        let mut it = st.keys();
-        assert_eq!(Some(&"android".into()), it.next());
-        assert_eq!(Some(&"blackberry".into()), it.next());
-        assert_eq!(Some(&"iphone".into()), it.next());
+        {
+            // mutuable borrow
+            let mut it = st.keys();
+            assert_eq!(Some(&"android".into()), it.next());
+            assert_eq!(Some(&"blackberry".into()), it.next());
+            assert_eq!(Some(&"iphone".into()), it.next());
+        }
 
         st.delete_min();
         assert_eq!(2, st.len());
@@ -169,6 +201,22 @@ mod tests {
         assert_eq!(1, st.len());
         assert_eq!(Some(&"blackberry".into()), st.min());
         assert_eq!(Some(&"blackberry".into()), st.max());
+    }
+
+    #[test]
+    fn keys_range() {
+        let mut st = BinarySearchST::<u32, u32>::new();
+        for i in 1..10 {
+            st.put(i, i * 2);
+        }
+        let v = st.keys_range(&1, &3).collect::<Vec<_>>();
+        assert_eq!([&1, &2, &3], &v[..]);
+        let v = st.keys_range(&0, &3).collect::<Vec<_>>();
+        assert_eq!([&1, &2, &3], &v[..]);
+        let v = st.keys_range(&8, &9).collect::<Vec<_>>();
+        assert_eq!([&8, &9], &v[..]);
+        let v = st.keys_range(&8, &20).collect::<Vec<_>>();
+        assert_eq!([&8, &9], &v[..]);
     }
 
     #[test]
