777d1a6082e2895c5a4fb28ca052e732aa511f73
simshi <simonshi@gmail.com>
2020-02-11 18:32:18 +0800

    draft symbol graph

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -2,10 +2,12 @@ mod cc;
 mod cycle;
 mod finder;
 mod graph;
+mod symbol_graph;
 mod union_find;
 
 pub use self::cc::*;
 pub use self::cycle::*;
 pub use self::finder::*;
 pub use self::graph::*;
+pub use self::symbol_graph::*;
 pub use self::union_find::*;
---
+++ src/graph/symbol_graph.rs
@@ -0,0 +1,115 @@
+use std::collections::HashMap;
+
+use super::Graph;
+
+pub struct SymbolGraph {
+    st: HashMap<String, usize>,
+    keys: Vec<String>,
+    g: Graph,
+}
+impl SymbolGraph {
+    pub fn new<'a, T>(edges: T) -> Self
+    where
+        T: Iterator<Item = &'a (String, String)>,
+    {
+        let mut st = HashMap::new();
+        let mut keys = Vec::new();
+        let mut indexed_edgeds = Vec::new();
+
+        for (v, w) in edges {
+            let vi = Self::get_or_add(&mut st, &mut keys, v);
+            let wi = Self::get_or_add(&mut st, &mut keys, w);
+            indexed_edgeds.push((vi, wi));
+        }
+
+        let size = st.len();
+        let mut sg = SymbolGraph {
+            st,
+            keys,
+            g: Graph::new(size),
+        };
+        for (v, w) in indexed_edgeds {
+            sg.g.add_edge(v, w);
+        }
+
+        sg
+    }
+
+    pub fn v_size(&self) -> usize {
+        self.g.v_size()
+    }
+    pub fn e_size(&self) -> usize {
+        self.g.e_size()
+    }
+
+    pub fn adj<'a>(&'a self, key: &String) -> Iter<'a> {
+        let v = self.st.get(key).unwrap();
+        Iter {
+            iter: self.g.adj(*v).cloned().collect::<Vec<usize>>().into_iter(),
+            keys: &self.keys,
+        }
+    }
+}
+
+// private methods
+impl SymbolGraph {
+    fn get_or_add(st: &mut HashMap<String, usize>, keys: &mut Vec<String>, key: &String) -> usize {
+        match st.get(key) {
+            None => {
+                let i = keys.len();
+                st.insert(key.clone(), i);
+                keys.push(key.clone());
+                i
+            }
+            Some(&v) => v,
+        }
+    }
+}
+pub struct Iter<'a> {
+    iter: std::vec::IntoIter<usize>,
+    keys: &'a Vec<String>,
+}
+impl<'a> Iterator for Iter<'a> {
+    type Item = &'a String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next().map(|x| &self.keys[x])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty() {
+        let edges = Vec::<(String, String)>::new();
+        let sg = SymbolGraph::new(edges.iter());
+        assert_eq!(0, sg.v_size());
+        assert_eq!(0, sg.e_size());
+    }
+
+    #[test]
+    fn basic_ops() {
+        let edges = vec![
+            (String::from("JFK"), String::from("MCO")),
+            (String::from("JFK"), String::from("ATL")),
+        ];
+        let sg = SymbolGraph::new(edges.iter());
+        assert_eq!(3, sg.v_size());
+        assert_eq!(2, sg.e_size());
+
+        let mut a1 = sg.adj(&String::from("JFK")).collect::<Vec<_>>();
+        a1.sort_unstable();
+        assert_eq!(vec![&String::from("ATL"), &String::from("MCO")], a1);
+
+        assert_eq!(
+            vec![&String::from("JFK")],
+            sg.adj(&String::from("ATL")).collect::<Vec<_>>()
+        );
+        assert_eq!(
+            vec![&String::from("JFK")],
+            sg.adj(&String::from("MCO")).collect::<Vec<_>>()
+        );
+    }
+}
